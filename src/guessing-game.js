a48cff8cb99159820549ae21a63e0d75af02eeb6
webjsmaster <web.js.master@gmail.com>
2023-09-10 17:53:37 +0300

    feat: implement $(Get-Date)

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.selector = null;
+        this.min = null;
+        this.max = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.selector = Math.ceil((this.min + this.max) / 2);
+        return this.selector;
     }
 
     lower() {
-
+        this.max =  this.selector;
     }
 
     greater() {
-
+        this.min = this.selector;
     }
 }
 
